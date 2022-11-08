953b05aae45560506b97253ce5bd3199dec8b8b4
ZynCitrus <115038252+ZynCitrus@users.noreply.github.com>
2022-11-08 15:15:57 +0100

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,19 @@
+echo
+
+pause
+
+start https://elevera.itslearning.com/
+
+start https://web.snapchat.com/
+
+start https://postmodernistx.github.io/youthful-dinosaurus/
+
+start https://stackoverflow.com/
+
+start https://reddit.com/
+
+start https://github.com/Medieinstitutet/fed22d-js-grundkurs-gruppuppgift-webbshop-skull-commando-squad
+
+start https://soundcloud.com
+
+start https://www.w3schools.com/
